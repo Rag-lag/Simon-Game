ca45b6365abf37b972862a44bead660b42304205
Raghav Pande <86782391+Rag-lag@users.noreply.github.com>
2022-01-20 00:55:11 +0300

    Uploading all the files

---
+++ game.js
@@ -0,0 +1,82 @@
+// Declaring variables
+var randomNumber = -1;
+var buttonColours = ["red", "blue", "green", "yellow"];
+var randomChosenColour = "";
+var gamePattern = [];
+var userChosenColour = [];
+var level = 0;
+var started = false;
+// Animate the pressed btn
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function(){$("#" + currentColour).removeClass("pressed");}, 200);
+}
+function animateGameOver(){
+  $("body").addClass("game-over");
+  setTimeout(function(){$("body").removeClass("game-over");}, 200);
+}
+
+// PLaysound when btn pressed
+function playSound(name) {
+  var audio = new Audio("./sounds/" + name + ".mp3");
+  audio.play();
+}
+// Generate the next color
+function nextSequence() {
+  userChosenColour = [];
+  level++;
+  $("h1").text("level " + level);
+  randomNumber = Math.floor(Math.random() * 4);
+
+
+  randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  // animatePress(randomChosenColour);
+  $("." + randomChosenColour).fadeOut(100).fadeIn(100);
+  // fadOutIn(randomChosenColour);
+  playSound(randomChosenColour);
+
+}
+function restart(){
+  level=0;
+  started=false;
+  gamePattern=[];
+}
+// Check ans
+function chckAns(currLevel) {
+  if (userChosenColour[currLevel] === gamePattern[currLevel]) {
+    if (userChosenColour.length === gamePattern.length) {
+      // delay(1000);
+      setTimeout(nextSequence, 1000);
+    }
+    // console.log("success");
+  } else {
+    playSound("wrong");
+    animateGameOver();
+    $("h1").text("Game Over, Press Any Key to Restart");
+    restart();
+    // console.log("worng");
+  }
+
+}
+
+// Intial press
+$(document).keypress(function() {
+  if (started == false) {
+    nextSequence();
+    started = true;
+  }
+
+});
+
+// COntinues the game
+$(".btn").click(function(event) {
+  userChosenColour.push(event.target.id);
+  animatePress(event.target.id);
+  playSound(event.target.id);
+  console.log(gamePattern);
+  console.log(userChosenColour);
+  chckAns(userChosenColour.length - 1);
+
+});
