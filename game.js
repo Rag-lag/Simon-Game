2e1ca66bef8bc53f92f71b80196a6589f80b47b1
Raghav020401 <86782391+Rag-lag@users.noreply.github.com>
2022-01-20 16:41:49 +0300

    Adding Heading blinking feature
    When game is over or for the first time the heading blinks.

--- game.js
+++ game.js
@@ -6,23 +6,46 @@ var gamePattern = [];
 var userChosenColour = [];
 var level = 0;
 var started = false;
+var myInterval = setInterval(function() {
+  $("h1").fadeOut(200).fadeIn(500);
+}, 1000);
+
+
+
 // Animate the pressed btn
 function animatePress(currentColour) {
   $("#" + currentColour).addClass("pressed");
-  setTimeout(function(){$("#" + currentColour).removeClass("pressed");}, 200);
+  setTimeout(function() {
+    $("#" + currentColour).removeClass("pressed");
+  }, 200);
 }
-function animateGameOver(){
+
+
+// To stop heading blinking
+function myStopFunction() {
+  clearInterval(myInterval);
+}
+
+
+// Animate gameover heading
+function animateGameOver() {
   $("body").addClass("game-over");
-  setTimeout(function(){$("body").removeClass("game-over");}, 200);
+  setTimeout(function() {
+    $("body").removeClass("game-over");
+  }, 200);
 }
+
 
 // PLaysound when btn pressed
 function playSound(name) {
   var audio = new Audio("./sounds/" + name + ".mp3");
   audio.play();
 }
+
+
 // Generate the next color
 function nextSequence() {
+  myStopFunction();
   userChosenColour = [];
   level++;
   $("h1").text("level " + level);
@@ -32,41 +55,51 @@ function nextSequence() {
   randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
-  // animatePress(randomChosenColour);
   $("." + randomChosenColour).fadeOut(100).fadeIn(100);
-  // fadOutIn(randomChosenColour);
   playSound(randomChosenColour);
 
 }
-function restart(){
-  level=0;
-  started=false;
-  gamePattern=[];
+
+
+// Restart
+function restart() {
+  level = 0;
+  started = false;
+  gamePattern = [];
 }
+
+
 // Check ans
 function chckAns(currLevel) {
+  // success
   if (userChosenColour[currLevel] === gamePattern[currLevel]) {
     if (userChosenColour.length === gamePattern.length) {
-      // delay(1000);
       setTimeout(nextSequence, 1000);
     }
-    // console.log("success");
-  } else {
+  }
+  // gameover
+  else {
     playSound("wrong");
     animateGameOver();
-    $("h1").text("Game Over, Press Any Key to Restart");
+    $("h1").text("Game Over, Press Anywhere to Restart");
+    myInterval = setInterval(function() {
+      $("h1").fadeOut(200).fadeIn(500);
+    }, 1000);
     setTimeout(restart, 1000);
-    // console.log("worng");
   }
 
 }
 
 // Intial press
+
 $(document).click(function() {
+
   if (started == false) {
+    myStopFunction();
     nextSequence();
     started = true;
   }
+
 
 });
 
